debe7e561fbd0e994916808127ab3a09a652be05
jared-v-samonte <jared.v.samonte@gmail.com>
2022-02-25 22:43:18 -0600

    git ignore updated correctly

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/JAVSamonte/Documents/Github/take_home_test
-# Build directory: /mnt/c/Users/JAVSamonte/Documents/Github/take_home_test/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(unit_tests "unit_tests")
-set_tests_properties(unit_tests PROPERTIES  _BACKTRACE_TRIPLES "/mnt/c/Users/JAVSamonte/Documents/Github/take_home_test/CMakeLists.txt;54;add_test;/mnt/c/Users/JAVSamonte/Documents/Github/take_home_test/CMakeLists.txt;0;")
